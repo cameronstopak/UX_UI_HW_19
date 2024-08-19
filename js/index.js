6aa5ac86dffb2dbfd4214b52dd2d2f6683117985
cameronstopak <cstopak01@gmail.com>
2024-08-19 14:57:43 -0400

    Final JS

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-$("About").click("About");
+document.addEventListener("DOMContentLoaded", function() { 
+  document.getElementById("Work").addEventListener("click", function() { 
+    window.scrollTo({ top: document.getElementById("WorkSection").offsetTop, behavior: "smooth" 
+    }); })
+  
+
+    document.getElementById("Skills").addEventListener("click", function() { 
+      window.scrollTo({ top: document.getElementById("SkillsSection").offsetTop, behavior: "smooth" 
+      }); })
+
+      document.getElementById("About").addEventListener("click", function() { 
+        window.scrollTo({ top: document.getElementById("HeroContainer").offsetTop, behavior: "smooth" 
+        }); })
+  
+
+
+      document.getElementById("Contact").addEventListener("click", function() { 
+        window.scrollTo({ top: document.getElementById("FooterSection").offsetTop, behavior: "smooth" 
+        }); });
+      
+      });
+
